75f0cad65d15d40d541224e5341d30af9641ae36
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-17 13:50:58 +0200

    noob app

---
+++ noob/src/App.tsx
@@ -0,0 +1,13 @@
+import { MyButton } from "./components/buttons"
+
+function App() {
+  
+  return (<div>
+        <h1> HELLO </h1>
+        <p>blablabla</p>
+        <MyButton></MyButton>
+      </div>
+  );
+}
+
+export default App
