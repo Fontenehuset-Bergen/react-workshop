ad1e4e6e1c9f253e4319ccb902a4ce2e09a28cd5
AndyKodehode <andykodehode@gmail.com>
2025-09-19 12:02:18 +0200

    vite fil er viktig

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MyButton } from './components/ui/buttons/buttons'
+import './assets/global.css'
 
 
 createRoot(document.getElementById('root')!).render(
