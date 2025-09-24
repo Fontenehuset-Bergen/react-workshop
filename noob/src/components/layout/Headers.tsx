55e6126e86916253f8fc51dd948ef9363e70804d
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-24 13:02:56 +0200

    making footer

--- noob/src/components/layout/Headers.tsx
+++ noob/src/components/layout/Headers.tsx
@@ -3,12 +3,12 @@ import navigationLinks from "../../data/navigationLinks.json"
 export function HeaderLayout() {
     
   return(
-    <header className="flex flex-row justify-around bg-gradient-to-1 from-orange-400 to-purple-500">
+    <header className="fixed flex flex-row justify-around bg-gradient-to-1 from-orange-400 to-purple-500">
         <span id="blabla" className="blabla">
             <img src="/vite.svg" />
         </span>
         <nav className="flex flex-row gap-4">
-          {navigationLinks.map((link)  => <a>{link.label}</a>)}
+          {navigationLinks.map((link)  => <a href = {link.href}>{link.label}></a>)}
         </nav>
     </header>
     );
--- noob/src/main.tsx
+++ noob/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { HeaderLayout } from './components/layout/Headers.tsx'
+import { FooterLayout } from './components/layout/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HeaderLayout />
     <App />
+    <FooterLayout />
   </StrictMode>,
 )
