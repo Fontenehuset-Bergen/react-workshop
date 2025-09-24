50dd61a8934c087a3b66e3aa224d3a1e3c2ef1c4
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-24 12:10:45 +0200

    gjenbrukbare links

--- noob/src/components/layout/Headers.tsx
+++ noob/src/components/layout/Headers.tsx
@@ -1,17 +1,14 @@
+import navigationLinks from "../../data/navigationLinks.json"
+
 export function HeaderLayout() {
-    const links = ["home", "about"] 
-
-    const linksnumber = links.map((link, index) => link + index)
-
-    console.log(links,linksnumber)
-
+    
   return(
     <header className="flex flex-row justify-around bg-gradient-to-1 from-orange-400 to-purple-500">
         <span id="blabla" className="blabla">
             <img src="/vite.svg" />
         </span>
         <nav className="flex flex-row gap-4">
-          {links.map((link)  => <a>{link}</a>)}
+          {navigationLinks.map((link)  => <a>{link.label}</a>)}
         </nav>
     </header>
     );
