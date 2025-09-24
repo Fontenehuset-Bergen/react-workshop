caff752680b9efb152976b54baf1178d12cf3313
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-24 13:50:27 +0200

    formatting section

--- noob/src/components/ui/cards/PoduceCards.tsx
+++ noob/src/components/ui/cards/PoduceCards.tsx
@@ -10,7 +10,7 @@ interface ProduceCardProps {
 export function ProduceCard(props: ProduceCardProps) {
     const { name, description, picture, price, qnt, tags } = props;
 
-    return <div>
+    return (<div className="flex gap-2 p-2 bg-amber-100 rounded">
         <div>
             <img src ={picture} alt={"A picture of " + name} />        
         </div>
@@ -20,8 +20,9 @@ export function ProduceCard(props: ProduceCardProps) {
             <p>Koster {price},-</p>
             <p>Vi har {qnt} igjen på lager</p>
             <span>
-                {tags.map((tag)=><p>{tag}</p>)}
+                {tags.map((tag)=><p className="bg-green-300 p-2 rounded">{tag}</p>)}
                 </span>
         </div>
     </div>
+    );
 }
--- noob/src/main.tsx
+++ noob/src/main.tsx
@@ -7,6 +7,7 @@ import { HeaderLayout } from './components/layout/Headers.tsx'
 import { SectionLayout } from './components/layout/Sections.tsx'
 import { FooterLayout } from './components/layout/Footer.tsx'
 import './index.css'
+import { ProduceCard } from "./components/ui/cards/PoduceCards.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,16 @@ createRoot(document.getElementById('root')!).render(
     <SectionLayout>
       <h1>section header</h1>
       <p>zippidy doo dah</p>
+    </SectionLayout>
+    <SectionLayout>
+      <ProduceCard
+      name="banan"
+      description="quite juicy yellow banana"
+      picture="/vite.svg"
+      price={29}
+      qnt={10}
+      tags={["fruit","tasty"]}
+      />
     </SectionLayout>
     <MyButton />
    </main>
