06ab51cc207485d09eb4588d5613e833d7f4ef11
WalleSP <walle1998@live.no>
2025-09-24 10:50:48 +0200

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,18 @@ import { HeaderLink } from './components/links/HeaderLink';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <header className="flex flex-row justify-around bg-gradient-to-l from-orange-50 to-blue-50">
-      <span>
-        logo  
+    <header className="flex flex-row justify-around bg-gradient-to-l from-orange-400 to-pruple-50">
+      <span id="blabla" className="blabla">
+        logo
       </span>
       <nav className="flex flex-row gap-4">
         <HeaderLink href="#" label="Home" />
         <HeaderLink href="#" label="About Us" />
       </nav>
     </header>
-    <main><MyButton /></main>
+    <main>
+      <MyButton />
+    </main>
     <footer></footer>
   </StrictMode>
 );
