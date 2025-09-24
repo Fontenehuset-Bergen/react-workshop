08cd992e9eb5409fbba4fffac62b319a445bfb74
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-24 13:29:25 +0200

    paragraph

--- noob/src/main.tsx
+++ noob/src/main.tsx
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
    <main>
     <SectionLayout>
       <h1>section header</h1>
+      <p>zippidy doo dah</p>
     </SectionLayout>
     <MyButton />
    </main>
