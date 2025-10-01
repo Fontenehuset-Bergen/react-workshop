9bc9def478e5a99dda6aac55f40bece76d14e37b
Helga-Vi <katarina.jorgensen@gmail.com>
2025-10-01 13:10:14 +0200

    making more stopwatch

--- noob/src/components/feature/useRef.tsx
+++ noob/src/components/feature/useRef.tsx
@@ -3,17 +3,27 @@ import {useRef, useState} from "react";
 export function ExampleUserRef2() {
     const [stoppwatchTime, setStoppwatchTime] = useState<number>(0);
     const [timeNow, setTimeNow] = useState<number>(0);
-    const intervalRef = useRef(0)
+    const [seconds, setSeconds] = useState(0);
+    const intervalRef = useRef(0);
 
 
 function handleStart(){
-    setTimeNow(Date.now())
+    if (timeNow == 0) {
+    setTimeNow(Date.now());
+    }
 
     const interval = setInterval(()=>{
-        const newTime = Date.now()
-        setStoppwatchTime(newTime)
+        setSeconds((old) => old + 0.01)
     }, 10)
-    intervalRef.current = interval
+    intervalRef.current = interval;
+}
+
+function handleStop() {
+    clearInterval(intervalRef.current)
+}
+
+function handleReset() {
+    setSeconds(0)
 }
 
 let timeDiff = (stoppwatchTime - timeNow) / 10000
@@ -24,8 +34,8 @@ return (
         <p>{timeDiff.toFixed(3)}</p>
         <span>
             <button type="button" onClick={handleStart}>Start</button>
-            <button type="button">Stop</button>
-            <button type="button">Reset</button>
+            <button type="button" onClick={handleStop}>Stop</button>
+            <button type="button" onClick={handleReset}>Reset</button>
         </span>
     </div>
 )
--- noob/src/main.tsx
+++ noob/src/main.tsx
@@ -2,20 +2,18 @@ import "./index.css";
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { MyButton } from "./components/ui/buttons/buttons.tsx"
 import { HeaderLayout } from './components/layout/Headers.tsx'
 import { SectionLayout } from './components/layout/Sections.tsx'
 import { FooterLayout } from './components/layout/Footer.tsx'
 import './index.css'
-import { ProduceCard } from "./components/ui/cards/ProduceCard.tsx";
 import { UserProfileCard } from "./components/ui/cards/UserProfile.tsx";
-import { TodoList } from "./components/feature/TodoList.tsx";
+import { ExampleUserRef2 } from "./components/feature/useRef.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HeaderLayout />
    <main>
-    <TodoList initialValue ={["import useState", "Define initial value"]}/>
+    <ExampleUserRef2 />
     <UserProfileCard
       age={29}
       avatar="/vite.svg"
@@ -28,27 +26,6 @@ createRoot(document.getElementById('root')!).render(
       <h1>section header</h1>
       <p>zippidy doo dah</p>
     </SectionLayout>
-    <SectionLayout>
-      <ProduceCard
-      name="banan"
-      description="quite juicy yellow banana"
-      picture="/vite.svg"
-      price={29}
-      qnt={10}
-      tags={["fruit","tasty"]}
-      onSale={false}
-      />
-      <ProduceCard
-      name="orange"
-      description="round, sour and orange"
-      picture="/vite.svg"
-      price={45}
-      qnt={15}
-      tags={["fruit","tasty","round"]}
-      onSale={true}
-      />
-    </SectionLayout>
-    <MyButton />
    </main>
     <FooterLayout />
   </StrictMode>,
