e28d8e8bd36b812cadd6c84e7bac2df8122c6ad0
Helga-Vi <katarina.jorgensen@gmail.com>
2025-10-01 13:26:41 +0200

    widescreen

--- noob/src/components/feature/useEffect.tsx
+++ noob/src/components/feature/useEffect.tsx
@@ -4,15 +4,23 @@ import {useEffect, useState} from "react"
 export function ExampleUseEffect() {
     const [width, setWidth] = useState(0)
     const [height, setHeight] = useState(0)
+    const [isWidescreen, setisWidescreen] = useState(false)
 
     useEffect(() => {
         setWidth(window.innerWidth)
         setHeight(window.innerHeight)
+
+        if (window.innerHeight > window.innerWidth) {
+            setisWidescreen(false)
+        } else {
+            setisWidescreen(true)
+        }
     }, [])
 
     return <div className="bg-green-200 m-4 p-2">
             <h2>Use effect example</h2>
             <p>Width: {width}</p>
             <p>Height: {height}</p>
+            <p>Widescreen: {isWidescreen ? true:false}</p>
     </div>
 }
