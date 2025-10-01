823fd74186778a33c48417649f0f3f973a667f0c
Helga-Vi <katarina.jorgensen@gmail.com>
2025-10-01 13:39:13 +0200

    with event listener

--- noob/src/components/feature/useEffect.tsx
+++ noob/src/components/feature/useEffect.tsx
@@ -2,25 +2,40 @@
 import {useEffect, useState} from "react"
 
 export function ExampleUseEffect() {
-    const [width, setWidth] = useState(0)
-    const [height, setHeight] = useState(0)
-    const [isWidescreen, setisWidescreen] = useState(false)
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [isWidescreen, setisWidescreen] = useState(false);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         setWidth(window.innerWidth)
         setHeight(window.innerHeight)
 
-        if (window.innerHeight > window.innerWidth) {
+        /*if (window.innerHeight > window.innerWidth) {
             setisWidescreen(false)
         } else {
             setisWidescreen(true)
-        }
-    }, [])
+        } */
 
-    return <div className="bg-green-200 m-4 p-2">
+            setisWidescreen(window.innerHeight > window.innerWidth)
+
+    function handleResize() {
+        if (containerRef.current) {
+        setWidth(window.innerWidth)
+        setHeight(window.innerHeight)
+        }
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+}, [])
+
+    return (
+            <div className="bg-green-200 m-4 p-2">
             <h2>Use effect example</h2>
             <p>Width: {width}</p>
             <p>Height: {height}</p>
-            <p>Widescreen: {isWidescreen ? true:false}</p>
+            <p>Widescreen: {isWidescreen ? "true":"false"}</p>
     </div>
+    );
 }
