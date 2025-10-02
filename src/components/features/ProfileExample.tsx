f40f806d952e691baf7886b174ec3e5cb56dfc91
Kristoffer <kunkristoffer@gmail.com>
2025-10-02 16:13:08 +0200

    added small profile card example

---
+++ src/components/features/ProfileExample.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { ProfileCard, type Profile } from "../ui/cards/Profile";
+
+export function ProfileExample() {
+  const [profile, setProfile] = useState<Profile>({
+    age: 0,
+    avatar: "",
+    description: "",
+    hobbies: ["React", "Vite"],
+    name: "",
+  });
+
+  return (
+    <section>
+      <h2>Vi kan sitte opp enkle form bindings</h2>
+      <p>Prøv å skriv noe i feltene under</p>
+      <div className="flex flex-wrap gap-6">
+        <form className="flex-1 p-2 rounded-md bg-blue-100/25">
+          <label>
+            <p>Name</p>
+            <input
+              type="text"
+              placeholder="What is your name?"
+              onChange={(e) =>
+                setProfile((prev) => ({ ...prev, name: e.target.value }))
+              }
+              className="w-full p-1 bg-white border-black text-black rounded"
+            />
+          </label>
+          <label>
+            <p>Age</p>
+            <input
+              type="number"
+              placeholder="What is your age?"
+              min={0}
+              max={200}
+              onChange={(e) =>
+                setProfile((prev) => ({ ...prev, age: Number(e.target.value) }))
+              }
+              className="w-full p-1 bg-white border-black text-black rounded"
+            />
+          </label>
+        </form>
+        <span className="flex-1">
+          <ProfileCard {...profile} />
+        </span>
+      </div>
+    </section>
+  );
+}
