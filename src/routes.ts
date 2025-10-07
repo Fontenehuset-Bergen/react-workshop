f4845b3f051932bd2b22a534637f7987900e54cb
Kristoffer <kunkristoffer@gmail.com>
2025-10-07 17:13:52 +0200

    added a route handler

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { type RouteConfig, index, route} from "@react-router/dev/routes";
 
+/**
+ * Her kan vi ligge til flere ruter hvis vi trenger det. Du kan lese mer her
+ * https://reactrouter.com/start/framework/routing
+ */
+
 export default [
   index("pages/home.tsx"),
   route(
     "/.well-known/appspecific/com.chrome.devtools.json",
-    "pages/debug-null.tsx"
+    "debug-null.tsx"
   ),
   route("*", "./not-found.tsx"),
 ] satisfies RouteConfig;
