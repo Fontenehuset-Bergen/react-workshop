ddc979cb27b25ae4b1a26243476ad660eb40abfb
Kristoffer <kunkristoffer@gmail.com>
2025-10-07 17:36:45 +0200

    added some placeholder pages we can use for testing navigation

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index, route} from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 /**
  * Her kan vi ligge til flere ruter hvis vi trenger det. Du kan lese mer her
@@ -7,9 +7,8 @@ import { type RouteConfig, index, route} from "@react-router/dev/routes";
 
 export default [
   index("pages/home.tsx"),
-  route(
-    "/.well-known/appspecific/com.chrome.devtools.json",
-    "debug-null.tsx"
-  ),
+  route("about", "pages/about/page.tsx"),
+  route("contact", "pages/contact/page.tsx"),
+  route("/.well-known/appspecific/com.chrome.devtools.json", "debug-null.tsx"),
   route("*", "./not-found.tsx"),
 ] satisfies RouteConfig;
