f86a328f5473799e8f88ba37ed8465010b087678
Helga-Vi <katarina.jorgensen@gmail.com>
2025-10-08 11:25:01 +0200

    css back

--- src/root.tsx
+++ src/root.tsx
@@ -1,3 +1,24 @@
+import "./assets/styles/index.css"
+import { Links, Meta, Scripts, ScrollRestoration } from "react-router";
+
+export function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+        <ScrollRestoration />
+      </body>
+    </html>
+  );
+}
+
 export default function App() {
     return <h1>hello world</h1>
 }
