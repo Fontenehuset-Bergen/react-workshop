c024d352164eef15f690b355fe7742a65fa48df0
Kristoffer <kunkristoffer@gmail.com>
2025-10-08 19:20:52 +0200

    added layout route example

--- src/components/ui/menus/navigation/Desktop.tsx
+++ src/components/ui/menus/navigation/Desktop.tsx
@@ -1,12 +1,19 @@
 import { type NavigationData } from "@/types/navigation";
+import { NavLink } from "react-router";
 
 export function NavBarDesktop({ links }: { links: NavigationData[] }) {
   return (
-    <nav className="flex gap-2">
+    <nav className="flex gap-4">
       {links.map((item) => (
-        <a key={item.label} href={item.href} className="!text-white hover:underline">
+        <NavLink
+          key={item.label}
+          to={item.href}
+          className={({ isActive }) =>
+            `!text-white ${isActive ? "!text-yellow-400 !underline" : ""}`
+          }
+        >
           {item.label}
-        </a>
+        </NavLink>
       ))}
     </nav>
   );
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,10 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
 /**
  * Her kan vi ligge til flere ruter hvis vi trenger det. Du kan lese mer her
@@ -9,6 +15,13 @@ export default [
   index("pages/home.tsx"),
   route("about", "pages/about/page.tsx"),
   route("contact", "pages/contact/page.tsx"),
+  ...prefix("faq", [
+    layout("components/layout/sidebar.tsx", [
+      index("pages/faq/faqIndex.tsx"),
+      route("react", "pages/faq/sections/react.tsx"),
+      route("router", "pages/faq/sections/router.tsx"),
+    ]),
+  ]),
   route("/.well-known/appspecific/com.chrome.devtools.json", "debug-null.tsx"),
   route("*", "./not-found.tsx"),
 ] satisfies RouteConfig;
