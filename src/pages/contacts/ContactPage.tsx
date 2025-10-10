f81478dd5cbdfaef0f2881ecf65edaf685c89b23
Helga-Vi <katarina.jorgensen@gmail.com>
2025-10-10 13:04:36 +0200

    nye sider

--- src/pages/contacts/ContactPage.tsx
+++ src/pages/contacts/ContactPage.tsx
@@ -1,5 +1,31 @@
 import type { Route } from "../+types/home"
+import contacts from "@/data/contactData.json"
 
-export function loader (){
+export async function loader({params}: Route.LoaderArgs) {
+    const result = contacts.find(query => query.username.toLowerCase() == params.username.toLowerCase())
 
+    return result
+}
+
+export default function ContactPage({ loaderData }: Route.ComponentProps) {
+    // loader klarte ikke å finne denne personen
+    if (!loaderData) {
+        return <h1>Unable to get details for this person</h1>
+    }
+
+    // Vi kan hente ut feltene vi trenger fra loaderData
+    const {firstname, lastname, bio,adress,age,city,phonenumber,postnumber,username,} = loaderData
+
+    return <div className="flex flex-col">
+        <h1>Contact page</h1>
+        <p>{firstname}</p>
+        <p>{lastname}</p>
+        <p>{age}</p>
+        <p>{username}</p>
+        <p>{phonenumber}</p>
+        <p>{adress}</p>
+        <p>{city}</p>
+        <p>{postnumber}</p>            
+        <p>{bio}</p>
+    </div>
 }
--- src/pages/contacts/layout.tsx
+++ src/pages/contacts/layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 import contacts from "@/data/contactData.json"
 
 export default function Layout() {
@@ -6,9 +6,38 @@ export default function Layout() {
         <main>
             <aside>
                 {contacts.map((person) => (
-                    
+                  <NavLink
+            to={"/contacts/" + person.username}
+            key={person.username}
+            className={({ isActive }) =>
+              `flex items-center gap-2 p-4 rounded-md ${isActive ? "bg-purple-200 hover:bg-purple-300" : "bg-white hover:bg-slate-100"}`
+            }
+          >
+            <img
+              src="/vite.svg"
+              alt="profile picture"
+              className="h-full aspect-square"
+            />
+            <span className="flex flex-col justify-center gap-2">
+              <span className="relative">
+                <p className="absolute -top-3 text-sm text-gray-600">name</p>
+                <p className="text-nowrap">
+                  {person.firstname} {person.lastname}
+                </p>
+              </span>
+              <span className="relative">
+                <p className="absolute -top-3 text-sm text-gray-600">
+                  username
+                </p>
+                <p> {person.username}</p>
+              </span>
+            </span>
+          </NavLink>  
                 ))}
             </aside>
+            <div className="flex-2">
+                <Outlet />
+            </div>
         </main>
     )
 }
