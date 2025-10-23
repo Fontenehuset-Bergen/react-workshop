ecb96d96fedc9e57af41adf379c6c1161012a05a
Kristoffer <kunkristoffer@gmail.com>
2025-10-23 17:49:10 +0200

    recreated pokedex from this weeks lesson

---
+++ src/pages/pokedex/PokedexLayout.tsx
@@ -0,0 +1,48 @@
+import type { Pokedex } from "@/types/pokemon";
+import { NavLink, Outlet, useLoaderData } from "react-router";
+
+export async function loader() {
+  console.log("fetching pokedex");
+  const response = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
+    { cache: "force-cache" }
+  );
+
+  if (!response.ok) {
+    throw new Response(`Unable to fetch pokedex`, {
+      status: response.status,
+      statusText: response.statusText,
+    });
+  }
+
+  const data: Pokedex = await response.json();
+  return data;
+}
+
+export default function PokemonLayout() {
+  const pokedex = useLoaderData<typeof loader>();
+
+  if (!pokedex) return {};
+
+  return (
+    <main>
+      <div className="flex gap-2 p-4 bg-red-500">
+        <div className="h-64 flex flex-col overflow-y-scroll p-2 bg-slate-400 text-black ">
+          {pokedex.results.map((pokemon, index) => (
+            <NavLink
+              key={pokemon.name}
+              to={`/pokedex/${pokemon.name}`}
+              className={({ isActive }) =>
+                `flex gap-2 py-1 px-2 ${isActive ? "bg-amber-600" : ""} !text-black`
+              }
+            >
+              <span className="w-10">{`#${index + 1}`}</span>
+              <span>{pokemon.name}</span>
+            </NavLink>
+          ))}
+        </div>
+        <Outlet />
+      </div>
+    </main>
+  );
+}
