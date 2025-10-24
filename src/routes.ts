98bbdcfcab977a209fa996b9068b45a8ee8b272d
Dragawarrior <Ellenblom22@gmail.com>
2025-10-24 12:13:12 +0200

    increased buffer size in git (pepe size)

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,5 @@
 import {
   index,
-  layout,
-  prefix,
   route,
   type RouteConfig,
 } from "@react-router/dev/routes";
@@ -10,12 +8,5 @@ export default [
   index("pages/home.tsx"),
   route("contact", "pages/contact/Page.tsx"),
   route("about", "pages/about/Page.tsx"),
-  ...prefix("faq", [
-    layout("components/layout/sidebar.tsx", [
-      index("pages/faq/faqIndex.tsx"),
-      index("react", "pages/faq/section/react.tsx"),
-      index("router", "pages/faq/sections/routes.tsx"),
-    ]),
-  ]),
   route("*", "not-found.tsx"),
 ] satisfies RouteConfig;
