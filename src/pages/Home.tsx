21f86abf02624b9073dc270a57fae1de18044682
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-11-07 13:02:30 +0100

    rwar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { HomepageHero } from '../components/feature/Hero'
 import '../assets/styles/example.css'
-import { badge} from "../../docs/1_introduction/assignment/easy/02_ternaries";
+// import { badge} from "../../docs/1_introduction/assignment/easy/02_ternaries";
 import * as operators from "../../docs/1_introduction/assignment/easy/03_operators_shortcircuit";
 
 
