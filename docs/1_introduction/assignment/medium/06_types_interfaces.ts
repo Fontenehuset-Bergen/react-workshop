e219b4054f19537f6eee5dde8086e58984aa26ae
Robert <robertandrelarsen@gmail.com>
2025-11-10 22:43:10 +0100

    Do medium 06_types_interface

--- docs/1_introduction/assignment/medium/06_types_interfaces.ts
+++ docs/1_introduction/assignment/medium/06_types_interfaces.ts
@@ -5,34 +5,42 @@
  */
 
 // 1) Lag en interface som passer for både person1 og person2 (tips: bruk valgfrie felter med ? der det trengs)
-interface Person {}
+interface Person {
+    name: string;
+    age?: number;
+    hobbies: string[];
+    isAdmin?: boolean;
+    joined: Date;
+}
 
 const person1: Person = {
-  name: "Åge",
-  age: 55,
-  hobbies: ["surfing", "sailing"],
-  isAdmin: true,
-  joined: new Date("2001-10-20"),
+    name: 'Åge',
+    age: 55,
+    hobbies: ['surfing', 'sailing'],
+    isAdmin: true,
+    joined: new Date('2001-10-20'),
 };
 const person2: Person = {
-  name: "Mia",
-  hobbies: ["reading"],
-  joined: new Date(),
+    name: 'Mia',
+    hobbies: ['reading'],
+    joined: new Date(),
 };
 
 // 2) Bruk interfaces inni interfaces
 interface Address {
-
+    city: string;
 }
 
 interface User {
-  
+    id: number;
+    name: string;
+    address?: Address;
 }
 
-const u1: User = { id: 1, name: "Ada" };
-const u2: User = { id: 2, name: "Linus", address: { city: "Bergen" } };
+const u1: User = { id: 1, name: 'Ada' };
+const u2: User = { id: 2, name: 'Linus', address: { city: 'Bergen' } };
 
 // 3) Lag en type hvor status kun kan være: idle, laoding, success eller error
-type Status = ""
+type Status = 'idle' | 'loading' | 'success' | 'error';
 
-const webpageStatus: Status = ""
+const webpageStatus: Status = 'success';
