27d74783d9fb208c92d1d24c65a1a868248625d5
Harald Nydal <haraldn.kodeloftet@gmail.com>
2025-11-25 17:12:16 +0100

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import { HomepageHero } from '../components/feature/Hero'
 import '../assets/styles/example.css'
 import { ChristmasCookieList, julekaker } from "./../components/feature/JuleBakstGalleri"
+
+import  defaultBackground  from  "./../../../public/images/background.jpg"
+import BackgroundImage, { animType } from "./../components/feature/BackgroundImage"
+ 
 // import { julekaker } from "./JuleBakstGalleri"
  
 function App() {
@@ -9,6 +13,7 @@ function App() {
 
   return(
   <>
+        <BackgroundImage cssClassName="backgroundImage" imageFile={defaultBackground} animNumber={animType.FadeIn} animDuration="1s"  />
         <ChristmasCookieList cookies={julekaker} />    
    </>
     )
