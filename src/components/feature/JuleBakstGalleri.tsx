ba941b4550c569e3f3c7031ee0395f025e8e9d8c
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-11-26 11:09:00 +0100

    test

--- src/components/feature/JuleBakstGalleri.tsx
+++ src/components/feature/JuleBakstGalleri.tsx
@@ -11,12 +11,14 @@ import grandmasImg from "/images/grandmas.jpg";
     hours : number
 }
 
-export interface christmasCookie {
+export interface christmasCookie 
+{
 
     id : number,
     name : string,
     time : cookingTime,
     toughness : "soft" | "medium" | "hard" | "unetable",
+    difficulty : "easy" | "medium" | "hard",
     oneOfTheSeven : boolean,
     imageFile : string
     
@@ -25,10 +27,10 @@ export interface christmasCookie {
 export const julekaker : christmasCookie[] = 
 [
     
-    { id:  1, name : "Pepperkake", time : { hours : 1, minutes : 32} , toughness : "medium", oneOfTheSeven : true, imageFile : pekkerkakeImg},
-    { id : 2, name : "Kakemann" , time : { hours : 0, minutes : 42 }, toughness : "hard", oneOfTheSeven : false, imageFile : kakemannImg},
-    { id : 3, name : "Grandmas", time : { hours : 3, minutes : 1 }, toughness : "unetable", oneOfTheSeven : false, imageFile : grandmasImg} ,
-    { id : 4 ,name : "Krumkake", time : { hours : 1, minutes: 22}, toughness :"soft", oneOfTheSeven: true, imageFile :  kromkakeImg },
+    { id:  1, name : "Pepperkake", time : { hours : 1, minutes : 32} , toughness : "medium", difficulty : "easy", oneOfTheSeven : true, imageFile : pekkerkakeImg},
+    { id : 2, name : "Kakemann" , time : { hours : 0, minutes : 42 }, toughness : "hard", difficulty : "hard", oneOfTheSeven : false, imageFile : kakemannImg},
+    { id : 3, name : "Grandmas", time : { hours : 3, minutes : 1 }, toughness : "unetable", difficulty: "medium", oneOfTheSeven : false, imageFile : grandmasImg} ,
+    { id : 4 ,name : "Krumkake", time : { hours : 1, minutes: 22}, toughness :"soft", difficulty : "easy", oneOfTheSeven: true, imageFile :  kromkakeImg },
 ];
 
 
@@ -37,8 +39,14 @@ function getTimeString(timeToCook : cookingTime ) : string
     return `${timeToCook.hours ? timeToCook.hours + " hours and" : ""}  ${timeToCook.minutes} minutes` ;
 }
 
+function getDifficultyEmoji({difficulty} : christmasCookie)
+{
+    if(difficulty === "easy") return "😀";
+    if(difficulty === "medium") return "😳";
+    return "🙁";
+}
 
-export function ChristmasCookieCard({id, name, time, toughness, oneOfTheSeven, imageFile} : christmasCookie)
+export function ChristmasCookieCard({id, name, time, toughness, difficulty,  oneOfTheSeven, imageFile} : christmasCookie)
 {
     // const isSevenText : string = oneOfTheSeven ? "yes" : "no";
 
@@ -52,6 +60,7 @@ export function ChristmasCookieCard({id, name, time, toughness, oneOfTheSeven, i
                 <p className="categoryText">Cookie name : <span className="cookieInfoText">{name}</span></p>
                 <p className="categoryText">Time to make :<span className="cookieInfoText">{getTimeString(time)}</span></p>  
                 <p className="categoryText">Hardness : <span className="cookieInfoText"> {toughness}</span></p>
+                <p className="categoryText">Difficulty : <span className="cookieInfoText"> {difficulty} <p>{ getDifficultyEmoji(difficulty);  }</p></span> </p>
                 <p className="categoryText">One of the seven : <span className="cookieInfoText"> {oneOfTheSeven ? "yes" : "no"}</span></p>
             </div>
             </>
@@ -68,12 +77,7 @@ export function ChristmasCookieList({ cookies} : { cookies  : christmasCookie[]}
             <div className="cookieDiv grid">
                 {cookies.map((currentCookie) =>
                     {
-                        let delay  = 0;
-                        const delayInc = 200;
-
-                       return <ChristmasCookieCard key={currentCookie.id} id={currentCookie.id} name={currentCookie.name} time={currentCookie.time} toughness={currentCookie.toughness} oneOfTheSeven={currentCookie.oneOfTheSeven} imageFile={currentCookie.imageFile}/>
-                
-
+                      return <ChristmasCookieCard key={currentCookie.id} id={currentCookie.id} name={currentCookie.name} time={currentCookie.time} toughness={currentCookie.toughness} difficulty={currentCookie.difficulty} oneOfTheSeven={currentCookie.oneOfTheSeven} imageFile={currentCookie.imageFile}/>
                     })
                 }
             </div>
