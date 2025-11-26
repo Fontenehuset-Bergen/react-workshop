4cd9fdbb9ba31e22afd887fc10053832a3238c18
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-11-26 09:01:32 +0100

    test

--- src/components/feature/JuleBakstGalleri.tsx
+++ src/components/feature/JuleBakstGalleri.tsx
@@ -1,29 +1,36 @@
 import React from "react";
-import  {christmasCookie } from "./julekakerdata.tsx";
+// import  {christmasCookie } from "./julekakerdata.tsx";
 
-
-// import { christmasCookie }  from "./julekakerdata";
-
-
+import pekkerkakeImg from "/images/pepperkake.jpg" ;
+import kakemannImg from "/images/kakemann.jpg";
+import kromkakeImg from "/images/kromkake.jpg"
+import grandmasImg from "/images/grandmas.jpg";
  
-//  export type cookingTime = {
-//     minutes : number,
-//     hours : number
-// }
+ export type cookingTime = {
+    minutes : number,
+    hours : number
+}
 
-// export interface christmasCookie {
+export interface christmasCookie {
 
-//     id : number,
-//     name : string,
-//     time : cookingTime,
-//     toughness : "soft" | "medium" | "hard" | "unetable",
-//     oneOfTheSeven : boolean,
-//     imageFile : string
+    id : number,
+    name : string,
+    time : cookingTime,
+    toughness : "soft" | "medium" | "hard" | "unetable",
+    oneOfTheSeven : boolean,
+    imageFile : string
     
-// }
+}
 
+export const julekaker : christmasCookie[] = 
+[
+    
+    { id:  1, name : "Pepperkake", time : { hours : 1, minutes : 32} , toughness : "medium", oneOfTheSeven : true, imageFile : pekkerkakeImg},
+    { id : 2, name : "Kakemann" , time : { hours : 0, minutes : 42 }, toughness : "hard", oneOfTheSeven : false, imageFile : kakemannImg},
+    { id : 3, name : "Grandmas", time : { hours : 3, minutes : 1 }, toughness : "unetable", oneOfTheSeven : false, imageFile : grandmasImg} ,
+    { id : 4 ,name : "Krumkake", time : { hours : 1, minutes: 22}, toughness :"soft", oneOfTheSeven: true, imageFile :  kromkakeImg },
+];
 
-// ikke brukt ennå
 
 function getTimeString(timeToCook : cookingTime ) : string 
 {
