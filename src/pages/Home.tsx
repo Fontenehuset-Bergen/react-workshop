79238a370f86d673c5baf718be17a6dc47169aeb
Kristoffer <kunkristoffer@gmail.com>
2025-12-05 09:34:06 +0100

    transfering files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,26 @@
 import { HomepageHero } from "../components/feature/Hero";
 import "../assets/styles/example.css";
 import JuleBakstGalleri from "../components/JuleBakstGalleri";
+import { MyButton } from "../components/ui/buttons/Button";
 // import Julebrus from "../components/Julebrus";
 
 function App() {
-    return (
-        <>
-            <HomepageHero />
-            <div className='card'>
-                <p>
-                    Endre og legg til i <code>src/App.tsx</code> legg til din
-                    egen komponent og se resultatet!
-                </p>
-            </div>
-            <div className='app-wrapper'>
-                <JuleBakstGalleri />
-                {/* <Julebrus /> */}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <HomepageHero />
+      <MyButton label="blambla" className="" />
+      <div className="card">
+        <p>
+          Endre og legg til i <code>src/App.tsx</code> legg til din egen
+          komponent og se resultatet!
+        </p>
+      </div>
+      <div className="app-wrapper">
+        <JuleBakstGalleri />
+        {/* <Julebrus /> */}
+      </div>
+    </>
+  );
 }
 
 export default App;
