ae2116dedae5fac9449f7c2748eb731da7e85951
Harald Nydal <haraldn.kodeloftet@gmail.com>
2025-12-08 08:36:28 +0100

    ferdig product page..json next

--- src/components/feature/Hero.tsx
+++ src/components/feature/Hero.tsx
@@ -11,7 +11,13 @@ export function HeroPage ()
       {/* <TextButton text="this is my button"/>
       <LinkButton text={"test link"} href="https://www.elkjop.no/" ></LinkButton>
       <Badge label="Out of Stock" warning="info"/> */}
-      <DisplayProduct  productName="Samsung s25" price="10 990 kr" description="Minste modellen i s25 serien. Oled skjerm, Snapdragon prosessor" inStock={true} imageUrl="src/assets/images/samsungs25.png"/>
+      <section className="productSection flex">
+        <DisplayProduct  productName="Samsung s25" price="10 990 kr" description="Minste modellen i s25 serien. Oled skjerm, Snapdragon prosessor" inStock={true} imageUrl="src/assets/images/samsungs25.png"/>
+      <DisplayProduct  productName="Samsung s23+" price="8 990 kr" description="Midterste modell i s23 serien. Oled skjerm, Snapdragon prosessor. 512 gb lagring" inStock={false} imageUrl="src/assets/images/samsungs23+.png"/>
+      <DisplayProduct  productName="Samsung s25 Ultra" price="15 990 kr" description="Topmodellen i s25 serien. Oled skjerm, Snapdragon prosessor. SPen medfølger" inStock={true} imageUrl="src/assets/images/samsungs25Ultra.png"/>
+      </section>
+     
+     
      </>
   );
 
