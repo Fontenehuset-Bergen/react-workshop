4cafd9081f0ea91eb14bb206caeb4d22d3ba8bdc
Kristoffer <kunkristoffer@gmail.com>
2025-12-08 10:14:46 +0100

    updated intersection example

--- src/components/examples/Intersection.tsx
+++ src/components/examples/Intersection.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useRef, useState, type ReactNode } from "react";
-
+/**
+ * Dette er et eksempel som Harald etterspurte hvor vi kan bruke intersection observer
+ * til å se om et element er synlig på skjermen. Vi kan bruke denne teknikken til å
+ * ligge til eller fjerne classes ut i fra hvor vi er på nettsiden. Du kan f.eks fade inn
+ * elementer når de blir synlig eller ligge på transitions slik at de får animasjoner.
+ */
 export function FadeSection({ children }: { children: ReactNode }) {
   const [isVisible, setIsVisisble] = useState<boolean>(false);
   const sectionRef = useRef<HTMLDivElement>(null);
