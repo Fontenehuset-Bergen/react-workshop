49816d5761687c8c5d0b18a191c077791cd4093b
Harald Nydal <haraldn.kodeloftet@gmail.com>
2025-12-09 10:23:43 +0100

    ferdig...lager to do gren

---
+++ src/components/feature/ToDo.tsx
@@ -0,0 +1,21 @@
+interface ToDo 
+{
+    mission : string,
+    isCompleded : boolean
+};
+
+export function ToDoList() 
+{
+    // const []
+
+     return(
+
+        <form className="inputForm">
+            <label htmlFor="">
+                <input type="text" />
+            </label>
+
+        </form>
+     )
+
+}
