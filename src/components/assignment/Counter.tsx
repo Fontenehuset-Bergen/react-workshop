aa306832e858adde5ae6645248b932b2cfe70d9e
Robert <robertandrelarsen@gmail.com>
2025-12-12 10:24:18 +0100

    Add Counter component

---
+++ src/components/assignment/Counter.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div className="counter">
+                <p>{count}</p>
+                <div className="controls">
+                    <button onClick={() => setCount(count + 1)}>ADD</button>
+                    <button onClick={() => setCount(count - 1)}>SUB</button>
+                    <button onClick={() => setCount(0)}>RESET</button>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,11 @@
-import { useState } from 'react'
-import { HomepageHero } from '../components/feature/Hero'
-import '../assets/styles/example.css'
+import { Counter } from '../components/assignment/Counter';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <HomepageHero />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Counter />
+        </>
+    );
 }
 
-export default App
+export default App;
