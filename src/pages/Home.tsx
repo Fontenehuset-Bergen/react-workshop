36e15b879d75a0f9df2f5e48782d0c61ef4c4a39
Kristoffer <kunkristoffer@gmail.com>
2025-12-19 09:49:30 +0100

    uploading classworks from wednesday

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,19 @@
 import "../assets/styles/example.css";
 import { useState } from "react";
 import { HomepageHero } from "../components/feature/Hero";
+import { MyFetch } from "../components/classwork/Effects";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [showEffectExample, setShowEffectExample] = useState(false);
 
   return (
     <>
       <HomepageHero />
-      <button type="button" onClick={() => setShowEffectExample((old) => !old)}>
-        {showEffectExample ? "close" : "open"} useEffect example
-      </button>
+      <MyFetch />
+      <MyFetch />
+      <MyFetch />
+      <MyFetch />
+      <MyFetch />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
